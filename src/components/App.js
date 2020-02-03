d1e016f115cd164e7650466101650ad7e4c42e59
AaronMullan <aaroncmullan@gmail.com>
2020-02-03 13:53:13 -0800

    one more time

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import Resty from '../containers/Resty';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 
-
 export default function App() {
   return (
     <>
